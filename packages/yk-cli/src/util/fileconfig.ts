7adbc10de2ebd0d331dea61e105a9603816f3c47
椛船 <jayson.zsj@alibaba-inc.com>
2019-06-16 20:22:24 +0800

    fix: fix fileconfig for right branch to install project

--- packages/yk-cli/src/util/fileconfig.ts
+++ packages/yk-cli/src/util/fileconfig.ts
@@ -3,7 +3,7 @@
  * 脚手架参数
  */
 export const config = {
-    ts_url: "https://raw.githubusercontent.com/zhusjfaker/egg-react-ssr/doc/addDoc/example/ssr-with-ts/package.json",
-    js_url: "https://raw.githubusercontent.com/zhusjfaker/egg-react-ssr/doc/addDoc/example/ssr-with-js/package.json",
-    branch: "doc/addDoc"
+    ts_url: "https://raw.githubusercontent.com/zhusjfaker/egg-react-ssr/backup/example/ssr-with-ts/package.json",
+    js_url: "https://raw.githubusercontent.com/zhusjfaker/egg-react-ssr/backup/example/ssr-with-js/package.json",
+    branch: "backup"
 }
