ad45a5c61e4969acadd4373e92be17f8f86dedd4
椛船 <jayson.zsj@alibaba-inc.com>
2019-06-18 15:30:40 +0800

    feat: Add Npm update module

--- packages/yk-cli/src/index.ts
+++ packages/yk-cli/src/index.ts
@@ -8,6 +8,7 @@ import { webpack } from "./webpackconfig";
 import { help } from "./help";
 import { cacheMange } from "./cache";
 import { component } from "./webcomponent";
+import { updatelocal } from "./update";
 
 /** 总会话函数 */
 const generator = (argv: string[]): void => {
@@ -19,30 +20,35 @@ const generator = (argv: string[]): void => {
 
     const session = async () => {
         let action = option.action;
-        switch (action) {
-            /** 构建项目 */
-            case "init":
-                /** 问询APP配置 */
-                await appconfig(option);
-                /** 构建应用 */
-                await init_app(option);
-                /** 缓存比对 */
-                await cacheMange(option);
-                /** 处理 package.json */
-                await packagejson(option);
-                /** 处理 webpack */
-                await webpack(option);
-                /** 处理 组件 */
-                if ((option.language === "javascript" && option.style !== "less") || (option.language === "typescript" && option.style !== "sass")) {
-                    await component(option);
-                }
-                console.log(`项目安装成功!`);
-                break;
-            /** 项目帮助 */
-            case "help":
-                await help();
-                break;
+        try {
+            switch (action) {
+                /** 构建项目 */
+                case "init":
+                    /** 自检更新当前脚手架是否最新 */
+                    await updatelocal(option);
+                    /** 问询APP配置 */
+                    await appconfig(option);
+                    /** 构建应用 */
+                    await init_app(option);
+                    /** 缓存比对 */
+                    await cacheMange(option);
+                    /** 处理 package.json */
+                    await packagejson(option);
+                    /** 处理 webpack */
+                    await webpack(option);
+                    /** 处理 组件 */
+                    if ((option.language === "javascript" && option.style !== "less") || (option.language === "typescript" && option.style !== "sass")) {
+                        await component(option);
+                    }
+                    console.log(`项目安装成功!`);
+                    break;
+                /** 项目帮助 */
+                case "help":
+                    await help();
+                    break;
+            }
         }
+        catch (ex) { }
     };
 
     session();
