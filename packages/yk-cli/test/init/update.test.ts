136d1a60dd47a3ecf03222dfafcdef1d1362bcea
zhangyuang <leonin0829@gmail.com>
2019-08-23 01:15:45 +0800

    test: 完善测试

--- packages/yk-cli/test/init/update.test.ts
+++ packages/yk-cli/test/init/update.test.ts
@@ -1,11 +1,18 @@
 jest.mock('fs')
+
 import { updateCli } from '../../src/update'
 
 const packageJson = require('../../package')
 const fs = require('fs')
-console.log('xx',updateCli)
+const MOCK_FILE_INFO = {
+  '../../package.json': '{version: 1.0.0}'
+}
+beforeEach(() => {
+  fs.__setMockFiles(MOCK_FILE_INFO)
+})
 test('test update cli', async () => {
-  packageJson.version = '1.0.0'
-  fs.writeFileSync('../../package.json', JSON.stringify(packageJson))
-  // await updateCli()
+  const text = fs.readFileSync('../../package.json')
+  console.log(text)
+  // fs.writeFileSync('../../package.json', JSON.stringify(packageJson))
+  // const std = await updateCli()
 })
