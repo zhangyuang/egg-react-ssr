d1aa1fa87aa5e5972f2bfc17c70438b758b0fce6
zhangyuang <leonin0829@gmail.com>
2019-08-25 23:18:37 +0800

    test

--- scripts/startServer.js
+++ scripts/startServer.js
@@ -2,7 +2,7 @@ const { exec, fork, spawn } = require('child_process')
 const { series, waterfall } = require('async')
 const npm = require('npm')
 
-exec('cd ./example/ssr-with-js', (err, stdout) => {
+exec('cd ./example/ssr-with-js && npm start', (err, stdout) => {
   if (err) {
     console.log('error', err)
     process.exit()
