a618e98b7bd3ce6f5ce66408b098101c968d3e57
zhangyuang <leonin0829@gmail.com>
2019-09-02 14:29:58 +0800

    test: circleci

--- test/scripts/preInstall.js
+++ test/scripts/preInstall.js
@@ -5,18 +5,18 @@ const { promisify } = require('util')
 const fs = require('fs')
 const execWithPromise = promisify(exec)
 
-const install = async (path, shell) => {
-  try {
-    fs.statSync(path)
-  } catch (error) {
-    const { stdout } = await execWithPromise(shell)
-    console.log(stdout)
-  }
+const install = (path, shell) => {
+  fs.stat(path, async err => {
+    if (err) {
+      const { stdout } = await execWithPromise(shell)
+      console.log(stdout)
+    }
+  })
 }
 
 const preInstall = async () => {
-  install('./packages/yk-cli/node_modules', 'cd ./packages/yk-cli && npm i --registry=https://registry.npm.taobao.org && npm run build')
-  install('./example/ssr-with-js/node_modules', 'cd ./example/ssr-with-js && npm i --registry=https://registry.npm.taobao.org')
+  install('./packages/yk-cli/node_modules', 'cd ./packages/yk-cli && cnpm i && npm run build')
+  install('./example/ssr-with-js/node_modules', 'cd ./example/ssr-with-js && cnpm i')
 }
 
 preInstall().catch(err => {
