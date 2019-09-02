075a00c3dcf7330d5b21cacccc7f4a9e5a6a33c9
zhangyuang <leonin0829@gmail.com>
2019-09-02 14:14:52 +0800

    test: circleci

--- test/scripts/preInstall.js
+++ test/scripts/preInstall.js
@@ -5,13 +5,13 @@ const { promisify } = require('util')
 const fs = require('fs')
 const execWithPromise = promisify(exec)
 
-const install = (path, shell) => {
-  fs.stat(path, async err => {
-    if (err) {
-      const { stdout } = await execWithPromise(shell)
-      console.log(stdout)
-    }
-  })
+const install = async (path, shell) => {
+  try {
+    fs.statSync(path)
+  } catch (error) {
+    const { stdout } = await execWithPromise(shell)
+    console.log(stdout)
+  }
 }
 
 const preInstall = async () => {
