9d340462eba865dd97ae96bd198db7ea8198be2d
zhangyuang <leonin0829@gmail.com>
2019-09-03 10:10:25 +0800

    test

--- test/e2e/nightwatch.config.js
+++ test/e2e/nightwatch.config.js
@@ -1,14 +1,32 @@
 module.exports = {
   'src_folders': ['test/e2e/specs'],
-  'webdriver': {
+  'selenium': {
     'start_process': true,
-    'server_path': 'node_modules/.bin/chromedriver',
-    'port': 9515
+    'server_path': require('selenium-server').path,
+    'host': '127.0.0.1',
+    'port': 4444,
+    'cli_args': {
+      'webdriver.chrome.driver': require('chromedriver').path
+    }
   },
   'test_settings': {
     'default': {
+      'selenium_port': 4444,
+      'selenium_host': 'localhost',
+      'silent': true,
+      'screenshots': {
+        'enabled': true,
+        'on_failure': true,
+        'on_error': false,
+        'path': 'test/e2e/screenshots'
+      }
+    },
+
+    'chrome': {
       'desiredCapabilities': {
-        'browserName': 'chrome'
+        'browserName': 'chrome',
+        'javascriptEnabled': true,
+        'acceptSslCerts': true
       }
     }
   }
