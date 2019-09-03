1851cd03452b83c896773b1a663c1f6df5227ef0
zhangyuang <leonin0829@gmail.com>
2019-09-03 10:59:13 +0800

    test

--- test/e2e/startServer.js
+++ test/e2e/startServer.js
@@ -3,29 +3,32 @@
 const egg = require('egg')
 const path = require('path')
 const { exec, fork } = require('child_process')
+const { promisify } = require('util')
+const execWithPromise = promisify(exec)
 const baseDir = process.env.BASE_DIR = path.resolve(__dirname, '../../example/ssr-with-js/')
 
 egg.startCluster({
   baseDir: baseDir,
   port: 7001,
   workers: 1
-}, () => {
-  const child = fork('./packages/yk-cli/bin/clientRender')
-  child.send({
-    msg: 'start dev'
-  })
-  child.on('message', data => {
-    if (data.msg === 'start dev finish') {
-      exec('nightwatch --config ./test/e2e/nightwatch.config.js', (err, stdout) => {
-        if (err) {
-          console.log(err)
-          process.exit()
-        }
+}, async () => {
+  try {
+    await execWithPromise('cd ./example/ssr-with-js && npm run build:server')
+    const child = fork('./packages/yk-cli/bin/clientRender')
+    child.send({
+      msg: 'start dev'
+    })
+    child.on('message', async data => {
+      if (data.msg === 'start dev finish') {
+        const { stdout } = await execWithPromise('nightwatch --config ./test/e2e/nightwatch.config.js')
         console.log(stdout)
-      })
-    }
-  })
-  process.on('exit', () => {
-    child.kill()
-  })
+      }
+    })
+    process.on('exit', () => {
+      child.kill()
+    })
+  } catch (error) {
+    console.log('err', error)
+    process.exit()
+  }
 })
