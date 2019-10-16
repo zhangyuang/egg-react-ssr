e75f3db817d5aecf5f9afea1027ef58c0626781d
zhangyuang <leonin0829@gmail.com>
2019-10-16 15:11:56 +0800

    feat: 页面模版加上doctype html，完善ykcli

--- packages/yk-cli/src/renderLayout.ts
+++ packages/yk-cli/src/renderLayout.ts
@@ -1,51 +1,52 @@
 
 /* tslint:disable */
 
-import * as React from 'react';
-import { renderToString }from 'react-dom/server'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
 import path from 'path'
-import { webpackWithPromise } from './util'
 
-const fs = require('fs')
-const nodeExternals = require('webpack-node-externals');
-const cwd = process.env.BASE_DIR || process.cwd()
-const serverConfig = require(cwd + '/build/webpack.config.server')
-const isDev = process.env.NODE_ENV === 'development'
-let config: object
+let config:any
 
 try {
-  fs.statSync(cwd + `/config/config.ssr.js`)
-  config = require(cwd + `/config/config.ssr`)
+  config = require('../../../config/config.ssr')
 } catch (error) {
-  config = require(cwd + `/config/config.default`)
+  config = require('../../../config/config.default')
 }
 
-serverConfig.entry = {
-  Layout: cwd + '/web/layout'
+const isServerless = config.runtime === 'serverless'
+let serverConfig:any
+
+if (!process.env.FC_FUNC_CODE_PATH) {
+  const nodeExternals = require('webpack-node-externals')
+  serverConfig = require('../../../build/webpack.config.server')
+  serverConfig.entry = {
+    Layout: path.resolve(__dirname, '../../../web/layout')
+  }
+  serverConfig.output.path = path.resolve(__dirname, '../dist')
+  serverConfig.externals = nodeExternals({
+    whitelist: /\.(css|less|sass|scss)$/,
+    modulesDir: path.resolve(__dirname, '../../') // 保证寻找第三方模块的node_modules是当前应用的node_modules
+  })
 }
 
-serverConfig.output.path = path.resolve(__dirname, '../dist')
-serverConfig.externals = nodeExternals({
-  whitelist: /\.(css|less|sass|scss)$/,
-  modulesDir: path.resolve(__dirname, '../../') // 保证寻找第三方模块的node_modules是当前应用的node_modules
-})
-
-const reactToString = (Component: React.ComponentClass, props: object) => {
+const reactToStream = (Component: React.FunctionComponent, props: object) => {
   return renderToString(React.createElement(Component, props))
 }
 
 const renderLayout = async () => {
   let Layout
-
-  if (isDev) {
-    delete require.cache[path.resolve(__dirname, '../dist/Layout.server.js')]
-  }
-
   try {
-    Layout = require('../dist/Layout.server').default
+    // serverless 场景我们从事先构建好的应用目录下的dist文件夹中读取layout
+    Layout = isServerless ? require('../../../dist/Layout.server').default : require('../dist/Layout.server').default
   } catch (error) {
+    // 非serverless场景首次读取失败我们先调用webpack api构建一遍在ykcli的目录下再读取
+    const { webpackWithPromise } = require('./util')
     await webpackWithPromise(serverConfig)
-    Layout = require('../dist/Layout.server').default
+    try {
+      // 兼容serverless场景webpack静态分析打包错误
+      Layout = require('../dist/Layout.server').default
+    } catch (error) {
+    }
   }
 
   const props = {
@@ -56,10 +57,8 @@ const renderLayout = async () => {
     }
   }
 
-  const str = reactToString(Layout, props)
+  const str = reactToStream(Layout, props)
   return str
 }
 
 export default renderLayout
-
-
