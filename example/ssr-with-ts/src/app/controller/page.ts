50d95d96edc3081ffbf85d8d68d4b70c8e2a6899
文睿 <1342985759@qq.com>
2019-10-16 17:39:56 +0800

    feat：加入router.ts ，自动绑定前端路由

--- example/ssr-with-ts/src/app/controller/page.ts
+++ example/ssr-with-ts/src/app/controller/page.ts
@@ -8,9 +8,6 @@ export class Page {
 
   @inject()
   ctx: Context
-
-  @get('/')
-  @get('/news/:id')
   async index () {
     try {
       // Page为webpack打包的chunkName，项目默认的entry为Page
@@ -19,6 +16,26 @@ export class Page {
       Object.assign(this.ctx.app.config, ssrConfig)
       const stream = await renderToStream(this.ctx, this.ctx.app.config)
       this.ctx.body = stream
+    } catch (error) {
+      this.ctx.logger.error(`Page Controller renderToStream Error ${error}`)
+    }
+  }
+
+}
+@provide()
+@controller('/user')
+export class User {
+
+  @inject()
+  ctx: Context
+
+  @get('/')
+  async index () {
+    try {
+      // Page为webpack打包的chunkName，项目默认的entry为Page
+      this.ctx.type = 'text/html'
+      this.ctx.status = 200
+      this.ctx.body = 'hello user'
     } catch (error) {
       this.ctx.logger.error(`Page Controller renderToStream Error ${error}`)
     }
