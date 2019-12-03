36ee32d5b21f789be39795f2cd1c1fd9eea27903
zhangyuang <leonin0829@gmail.com>
2019-12-03 15:28:25 +0800

    chore: update

--- packages/ykfe-utils/src/getComponent.ts
+++ packages/ykfe-utils/src/getComponent.ts
@@ -1,8 +1,9 @@
 import { matchPath } from 'react-router-dom'
+import { RouteItem } from './interface/route'
 
-const getComponent = (Routes, path) => {
+const getComponent = (Routes: RouteItem[], path: string) => {
   // 根据请求的path来匹配到对应的component
-  const activeRoute = Routes.find(route => matchPath(path, route)) || {}
+  const activeRoute = Routes.find((route: RouteItem) => matchPath(path, route)) || {}
   const activeComponent = activeRoute.Component
   return activeComponent
 }
