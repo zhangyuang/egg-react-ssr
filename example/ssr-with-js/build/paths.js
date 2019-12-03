714395e0080531825881a242925d16bbf3c76a15
LeonCheung <569105585@qq.com>
2019-12-03 18:26:09 +0800

    test: perfect test (#115)
    * chore: 完善测试

--- example/ssr-with-js/build/paths.js
+++ example/ssr-with-js/build/paths.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
-const appDirectory = fs.realpathSync(process.cwd())
+const appDirectory = fs.realpathSync(path.resolve(__dirname, '..'))
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
 const envPublicUrl = process.env.PUBLIC_URL
