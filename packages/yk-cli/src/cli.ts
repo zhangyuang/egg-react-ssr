e9b0d671c1d4ddaaaf91bd0ef487aad497beec7e
zhangyuang <leonin0829@gmail.com>
2019-12-11 11:09:12 +0800

    test

--- packages/yk-cli/src/cli.ts
+++ packages/yk-cli/src/cli.ts
@@ -3,8 +3,11 @@
 import yargs from 'yargs'
 import init from './init'
 import { Optional } from './interface/option'
-import { Argv } from './interface/argv'
+// import { Argv } from './interface/argv'
 
+interface Argv extends yargs.Arguments {
+  appName: string
+}
 yargs
   .command('init [appName]', 'init the program', {}, async (argv: yargs.Arguments<Argv>) => {
     const option: Optional = {
