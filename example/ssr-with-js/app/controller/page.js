bf86232a04b62429ebc6c097b7dd16adf532413f
zhangyuang <leonin0829@gmail.com>
2019-12-13 00:12:57 +0800

    chore update

--- example/ssr-with-js/app/controller/page.js
+++ example/ssr-with-js/app/controller/page.js
@@ -1,6 +1,6 @@
 
 const Controller = require('egg').Controller
-const renderToStream = require('ykfe-utils/lib/renderToStream')
+const renderToStream = require('ykfe-utils/lib/renderToStream').default
 const ssrConfig = require('../../config/config.ssr')
 
 class PageController extends Controller {
