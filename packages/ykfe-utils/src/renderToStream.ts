024e19ffe5f06e2024b3f943265e7981947605a1
zhangyuang <leonin0829@gmail.com>
2020-01-17 23:22:41 +0800

    update

--- packages/ykfe-utils/src/renderToStream.ts
+++ packages/ykfe-utils/src/renderToStream.ts
@@ -1,23 +1,23 @@
 import { Context }from 'midway'
 import { useCdn } from './useCdn'
-import { Config }from './interface/config'
+import { Config,ServerJs }from './interface/config'
 import { Global }from './interface/global'
 
 declare const global: Global
 
 const renderToStream = async (ctx: Context, config: Config) => {
   const { useCDN, serverJs, baseDir } = config
-  const _baseDir = baseDir || process.cwd()
+  const BASE_DIR = baseDir || process.cwd()
   const isLocal = process.env.NODE_ENV === 'development' || config.env === 'local' // 标志非正式环境
-  let _serverJs = serverJs
+  let SEVER_JS = serverJs
 
-  if (useCDN) {
-    _serverJs = await useCdn(serverJs as string, isLocal)
+  if (useCDN && typeof serverJs === 'string') {
+    SEVER_JS = await useCdn(serverJs, isLocal)
   }
 
-  if (!useCDN && isLocal && typeof serverJs === 'string') {
+  if (isLocal && typeof SEVER_JS === 'string') {
     // 本地开发环境下每次刷新的时候清空require服务端文件的缓存，保证服务端与客户端渲染结果一致
-    delete require.cache[serverJs]
+    delete require.cache[SEVER_JS]
   }
 
   const csr = ctx.request?.query?.csr ? ctx.request.query.csr : false // 兼容express和koa的query获取
@@ -30,21 +30,11 @@ const renderToStream = async (ctx: Context, config: Config) => {
 
   if (!global.renderToNodeStream) {
     // for this issue https://github.com/ykfe/egg-react-ssr/issues/4
-    global.renderToNodeStream = require(_baseDir + '/node_modules/react-dom/server').renderToNodeStream
+    global.renderToNodeStream = require(BASE_DIR + '/node_modules/react-dom/server').renderToNodeStream
   }
 
-  if (!global.serverStream) {
-    global.serverStream = {}
-  }
+  const serverComponent = typeof SEVER_JS === 'string' ? require(SEVER_JS).default(ctx) : (SEVER_JS as ServerJs)(ctx)
 
-  let key: string = 'default'
-
-  if (isLocal || !global.serverStream[key]) {
-    key = typeof _serverJs === 'string' ? _serverJs : 'default'
-    global.serverStream[key] = typeof _serverJs === 'string' ? require(_serverJs).default : _serverJs
-  }
-
-  const serverComponent = await global.serverStream[key](ctx)
   const stream = global.renderToNodeStream(serverComponent)
 
   return stream
