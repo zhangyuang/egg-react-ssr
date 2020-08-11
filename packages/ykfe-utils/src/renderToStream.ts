dfe62ab78080c91a9afa5d4294c4dde2f9542ea3
功赫 <gonghe.wl@alibabapictures.com>
2020-08-11 17:17:40 +0800

    chore: return csr result early

--- packages/ykfe-utils/src/renderToStream.ts
+++ packages/ykfe-utils/src/renderToStream.ts
@@ -9,6 +9,15 @@ import { Global } from './interface/global'
 declare const global: Global
 
 const renderToStream = async (ctx: Context, config: Config) => {
+  const csr = ctx.request?.query?.csr ? ctx.request.query.csr : false // 兼容express和koa的query获取
+
+  if (config.type !== 'ssr' || csr) {
+    const stream = await renderLayout(ctx, config)
+    const doctypeStream = new ReadableString('<!DOCTYPE html>')
+    // @ts-ignore
+    return mergeStream(doctypeStream, stream)
+  }
+
   const { useCDN, serverJs, baseDir, isRax, useReactToString } = config
   const BASE_DIR = baseDir || process.cwd()
   const isLocal = process.env.NODE_ENV === 'development' || config.env === 'local' // 标志非正式环境
@@ -23,15 +32,6 @@ const renderToStream = async (ctx: Context, config: Config) => {
   if (isLocal && typeof SEVER_JS === 'string') {
     // 本地开发环境下每次刷新的时候清空require服务端文件的缓存，保证服务端与客户端渲染结果一致
     delete require.cache[SEVER_JS]
-  }
-
-  const csr = ctx.request?.query?.csr ? ctx.request.query.csr : false // 兼容express和koa的query获取
-
-  if (config.type !== 'ssr' || csr) {
-    const stream = await renderLayout(ctx, config)
-    const doctypeStream = new ReadableString('<!DOCTYPE html>')
-    // @ts-ignore
-    return mergeStream(doctypeStream, stream)
   }
 
   if (!global.renderToNodeStream) {
