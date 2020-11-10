152a61832b8fecfb808f8a792ccfc87215da2bb1
zhangyuang <leonin0829@gmail.com>
2020-11-10 14:22:08 +0800

    chore: ykcli init 保留迁移提示

--- packages/yk-cli/src/cli.ts
+++ packages/yk-cli/src/cli.ts
@@ -3,6 +3,9 @@
 import yargs from 'yargs'
 
 yargs
+  .command('init [appName]', 'init the program', {}, async () => {
+    console.log('ykcli init 命令已废弃，请参考 README.md 使用 npm init ssr-app my-ssr-project --template=ssr-with-js 来创建应用')
+  })
   .command('dev', 'start clientRender', {}, async () => {
     process.env.NODE_ENV = 'development'
     const { dev } = require('./clientRender')
